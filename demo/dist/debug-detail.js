dc3f35d945add50f89c70c98a839ec2e8cd6b05b
Lode Claassen <lode@alsvanzelf.nl>
2017-07-31 13:23:57 +0200

    improve accordion clickability

---
+++ demo/dist/debug-detail.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+	/**
+	 * make accordion headings clickable, not only the link inside them
+	 */
+	var accordionTitles = document.getElementsByClassName('panel-heading');
+	for (var i=0; i<accordionTitles.length; i++) {
+		var accordionTitle = accordionTitles[i];
+		accordionTitle.className = accordionTitle.className + ' clickable';
+		accordionTitle.addEventListener('click', function(event) {
+			var accordionLink = accordionTitle.querySelectorAll('[data-toggle="collapse"]')[0];
+			accordionLink.click();
+		});
+	}
+}
