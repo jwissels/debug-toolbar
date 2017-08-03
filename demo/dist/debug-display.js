6e855b045dbd56c664081951550ab56d25359444
Lode Claassen <lode@alsvanzelf.nl>
2017-08-03 16:18:39 +0200

    prevent errors when loaded without iframe

--- demo/dist/debug-display.js
+++ demo/dist/debug-display.js
@@ -9,26 +9,30 @@ setTimeout(function() {
  * allow the parent to close the detail
  * @see https://stackoverflow.com/a/251645/230422
  */
-// overwrite the placeholder
-window.parent.iframeCloseDebugCallback = function() {
-	// keep the animation calm by first closing the display completely
-	setTimeout(function() {
-		$('#detail-container, .detail-container').removeClass('detail-active');
-	}, 100);
-};
+if (window.parent.iframeCloseDebugCallback != undefined) {
+	// overwrite the placeholder
+	window.parent.iframeCloseDebugCallback = function() {
+		// keep the animation calm by first closing the display completely
+		setTimeout(function() {
+			$('#detail-container, .detail-container').removeClass('detail-active');
+		}, 100);
+	};
+}
 
 /**
  * shortcuts
  * - Shift+D: open/close display
  * - Escape:  close display
  */
-window.addEventListener('keydown', function(event) {
-	if (event.ctrlKey == false && event.altKey == false && event.shiftKey && event.keyCode == 68) {
-		event.preventDefault();
-		parent.window.closeDebugDisplay();
-	}
-	if (event.ctrlKey == false && event.altKey == false && event.shiftKey == false && event.keyCode == 27) {
-		event.preventDefault();
-		parent.window.closeDebugDisplay();
-	}
-});
+if (parent.window.closeDebugDisplay != undefined) {
+	window.addEventListener('keydown', function(event) {
+		if (event.ctrlKey == false && event.altKey == false && event.shiftKey && event.keyCode == 68) {
+			event.preventDefault();
+			parent.window.closeDebugDisplay();
+		}
+		if (event.ctrlKey == false && event.altKey == false && event.shiftKey == false && event.keyCode == 27) {
+			event.preventDefault();
+			parent.window.closeDebugDisplay();
+		}
+	});
+}
