90e0e2777629c03cbc3333ae777c2c8575ce29e4
Lode Claassen <lode@alsvanzelf.nl>
2017-08-04 20:01:09 +0200

    improve demo landing page

--- demo/index.php
+++ demo/index.php
@@ -18,7 +18,55 @@ error_reporting(-1);
  * run your application
  */
 
-usleep(100);
+try {
+	// supply working credentials + table to see PDO debugging
+	$pdo = new \PDO('mysql:host=127.0.0.1;dbname=debug_demo', 'debug_demo', 'debug_demo');
+	$pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+	
+	function executePDO($pdo, $query, $binds=[]) {
+		$statement = $pdo->prepare($query);
+		foreach ($binds as $key => $value) {
+			$statement->bindValue($key, $value);
+		}
+		$statement->execute();
+		
+		// pass every executed statement
+		Log::trackPDOExecutedStatement($statement, $binds);
+		
+		$record = $statement->fetch(\PDO::FETCH_ASSOC);
+		$statement->closeCursor();
+		return $record;
+	}
+	
+	$posts = [];
+	$posts[] = executePDO($pdo, "SELECT * FROM `posts` WHERE `id` = :id;", ['id'=>1]);
+	$posts[] = executePDO($pdo, "SELECT * FROM `posts` WHERE `id` = :id;", ['id'=>1]);
+	$posts[] = executePDO($pdo, "SELECT * FROM `posts` WHERE `id` = :id;", ['id'=>2]);
+	$posts[] = executePDO($pdo, "SELECT * FROM `posts` WHERE `id` = :id;", ['id'=>3]);
+	$posts[] = executePDO($pdo, "SELECT * FROM `posts` WHERE `title` = :title AND `active` = :active;", ['title'=>'Foo', 'active'=>1]);
+}
+catch (\Exception $e) {
+	// continue if sql is not configured
+}
+
+$loader  = new \Twig_Loader_Filesystem([__DIR__.'/templates']);
+$options = ['debug' => true];
+$twig    = new \Twig_Environment($loader, $options);
+
+if ($twig->isDebug()) {
+	$profiler = new \Twig_Profiler_Profile();
+	
+	// pass the profiler once during twig setup
+	Log::trackTwigProfiler($profiler);
+	
+	$twig->addExtension(new \Twig_Extension_Profiler($profiler));
+}
+
+$pageData = [];
+if (!empty($posts)) {
+	$pageData['posts'] = $posts;
+}
+$pageRendered = $twig->render('index.html', $pageData);
 
 /**
  * end of the request
@@ -35,19 +83,6 @@ $logId = Log::track($logger);
  * render the toggler in the template
  */
 $toggler = new Toggler($logId);
-?>
-<!DOCTYPE html>
-<html>
-<head>
-	<title>Demo</title>
-	<style type="text/css">
-		body {
-			background-color: lightblue;
-		}
-	</style>
-</head>
-<body>
-	<h1>Demo</h1>
-	<?php echo $toggler->render(); ?>
-</body>
-</html>
+$pageRendered = str_replace('</body>', $toggler->render().'</body>', $pageRendered);
+
+echo $pageRendered;
